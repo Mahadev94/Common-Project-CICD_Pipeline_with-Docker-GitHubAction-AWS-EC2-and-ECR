2b169d38b192e1254109c1eeacdf209f7ccef0fc
Mahadev Kharmate <mahadev.mk294@gmail.com>
2023-10-05 20:25:36 +0530

    changes to trigger CI-CD

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template,jsonify
 from source.pipeline.prediction_pipeline import PredictPipeline,CustomData
 
 application=Flask(__name__)
-
 app=application
 
 @app.route("/")
